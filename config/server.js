694464ab7bef58487263ef46c920a9aa86b8f32f
mazigh <madjid@malou.io>
2020-11-26 15:55:17 +0100

    create strapi project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '51268aab7423a22d79c4c71986697c73'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ca065c66-4af5-4f45-a402-70fdad020848'
+};
